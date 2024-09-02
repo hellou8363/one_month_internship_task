2b65aa657329c42bf0208f26db7d63637a42493f
eunhye han <hellou8363@gmail.com>
2024-09-02 16:19:52 +0900

    first commit

---
+++ src/main/java/org/zerock/one_month_internship_task/OneMonthInternshipTaskApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.one_month_internship_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMonthInternshipTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneMonthInternshipTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/one_month_internship_task/OneMonthInternshipTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.one_month_internship_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMonthInternshipTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
